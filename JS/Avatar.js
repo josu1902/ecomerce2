b0cb5ff0545c2d44dbacc873baa0bb8f2bdf0c83
= <josue2003elizondo@gmail.com>
2024-09-17 10:24:54 -0300

    prueba

---
+++ JS/Avatar.js
@@ -0,0 +1,8 @@
+const Avatar = () => {
+    return (
+        <div>
+            <img src="C:\Users\maria\OneDrive\Documentos\Proyecto.vila\E-Comerce\Avatar.jpeg" />
+        </div>
+    )
+}
+export default Avatar
---
+++ JS/Navbar.js
@@ -0,0 +1,16 @@
+import "/Navbar.css"
+//Import CartWidget from /cartwidget
+import Avatar from "../Avatar/Avatar"
+const Navbar =() =>{
+    return(
+        <><div>
+            <h1>Ecommerce</h1>
+        </div>
+        <div>
+            <button>Pasteles</button>
+            <button>Bebidas</button>
+            <button>Golosinas</button>
+        </div></>
+    )
+}
+export default Navbar
